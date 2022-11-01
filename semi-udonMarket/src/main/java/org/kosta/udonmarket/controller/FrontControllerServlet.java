e5f64ceed37ca2f4163ad667d5f9b7919d3cf6d0
parkpeom6788 <parkpeom6788@gmail.com>
2022-11-01 18:59:22 +0900

    박범 - 공통클래스 작업

--- semi-udonMarket/src/main/java/org/kosta/udonmarket/controller/FrontControllerServlet.java
+++ semi-udonMarket/src/main/java/org/kosta/udonmarket/controller/FrontControllerServlet.java
@@ -7,35 +7,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FrontControllerServlet
- */
 @WebServlet("*.do")
 public class FrontControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public FrontControllerServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    protected void doDispatch(HttpServletRequest request ,HttpServletResponse response) throws ServletException , IOException {
+    	try {
+    		String servletPath = request.getServletPath();
+    		String controllerName = servletPath.substring(1, servletPath.lastIndexOf("."));
+			Controller controller = HandlerMapping.getInstance().create(controllerName);
+			
+			String viewPath = controller.handleRequest(request, response);
+    	
+			if(viewPath.startsWith("redirect:")) {
+				response.sendRedirect(viewPath.substring(9));
+			} else {
+				request.getRequestDispatcher(viewPath).forward(request, response);
+			}
+    	} catch (Exception e) {
+    		response.sendRedirect("error.jsp");
+    		e.printStackTrace();
+    	}
+    }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		this.doDispatch(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("utf-8");
+		this.doDispatch(request, response);
 	}
 
 }
